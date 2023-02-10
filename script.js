ce2c69567f758ed0209dfaf68edfd124b2f7a3cc
Khushboo Kasnia <101094189+khushi230@users.noreply.github.com>
2023-02-10 18:22:32 +0530

    can updated price according to movie and seats

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ const seat = document.querySelectorAll(".row .seat :not(.occupied");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelected = document.getElementById("movie");
-const ticketPrice = parseInt(movieSelected.value);
+let ticketPrice = parseInt(movieSelected.value);
+
+const updateCount = () => {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const updatedCount = selectedSeats.length;
+  count.innerHTML = updatedCount;
+  total.innerHTML = updatedCount * ticketPrice;
+};
+
+movieSelected.addEventListener("change", (e) => {
+  ticketPrice = e.target.value;
+  updateCount();
+});
 
 container.addEventListener("click", (e) => {
-  console.log(e);
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("occupied")
   ) {
     e.target.classList.toggle("selected");
   }
+  updateCount();
 });
